2d618d88b722f79b1fb4d8da413eb8b157bc947d
Russell <Russell@Russells-MacBook-Pro.local>
2018-04-06 08:21:31 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var app = express();
+var appUrl = 'localhost:3000/';
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/urlshort', function(err, database) {
+    
+    var db = database.db('urlshortdb')
+    app.get('/new/:longUrl(*)', function(req, res) {
+        var longUrl = req.params.longUrl;
+        var uniqueId = new Date().getTime();
+        uniqueId = uniqueId.toString();
+        uniqueid = uniqueId.slice(0, -2);
+        var shortUrl = appUrl + String(uniqueId);
+
+        db.collection('urlshort').insert({
+            "longUrl" : longUrl,
+            "shortUrl" : shortUrl,
+            "uniqueId" : uniqueId
+        }, function(err, data) {
+            if (err) console.log('Error inserting into database');
+            var l = data.ops[0].longUrl;
+            var s = data.ops[0].shortUrl;
+            var obj = {"original_url" : l, "short_url" : s}
+            res.send(obj);
+        })
+    });
+
+    app.get("/:uniqueId", function(req, res) {
+        var uniqueId = req.params.uniqueId;
+
+        db.collection('urlshort').find({
+            "uniqueId" : uniqueId
+        }).toArray(function(err, data) {
+            if (err) console.log('Error finding shortened URL');
+            if (data.length == 0) {
+                console.log('No short URL matching this one in the database')
+            }
+            console.log(data[0].longUrl);
+            res.redirect(data[0].longUrl);
+        });
+    });
+}); //end MongoDB connection
+
+var port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('Server listening!');
+});
