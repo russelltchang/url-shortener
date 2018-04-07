d1c43a244cc77dc169f0f978c88f6b683f476a9f
Russell <Russell@Russells-MacBook-Pro.local>
2018-04-07 13:54:44 -0700

    database syntax edit

--- app.js
+++ app.js
@@ -1,56 +1,58 @@
 var express = require('express');
 var app = express();
+var url = 'mongodb://russelltchang:hackstack13!@ds237409.mlab.com:37409/urlshortdb'
 var appUrl = 'https://lit-headland-86744.herokuapp.com/';
 var MongoClient = require('mongodb').MongoClient;
 var path = require('path');
-
-MongoClient.connect('mongodb://russelltchang:hackstack13!@ds237409.mlab.com:37409/urlshortdb', function(err, database) {
 
     app.get('/', function(req, res) {
         res.sendFile(path.join(__dirname + '/public/index.html'));
     });
 
     app.get('/new/:longUrl(*)', function(req, res) {
-        var db = database.db('urlshortdb');
-        var longUrl = req.params.longUrl;
-        var uniqueId = new Date().getTime();
-        uniqueId = uniqueId.toString();
-        uniqueid = uniqueId.slice(0, -2);
-        var shortUrl = appUrl + String(uniqueId);
+        MongoClient.connect(url, function(err, database) {
+            var db = database.db('urlshortdb');
+            var longUrl = req.params.longUrl;
+            var uniqueId = new Date().getTime();
+            uniqueId = uniqueId.toString();
+            uniqueid = uniqueId.slice(0, -2);
+            var shortUrl = appUrl + String(uniqueId);
 
-        db.collection('urlshort').insert({
-            "longUrl" : longUrl,
-            "shortUrl" : shortUrl,
-            "uniqueId" : uniqueId
-        }, function(err, data) {
-            if (err) console.log('Error inserting into database');
-            var l = data.ops[0].longUrl;
-            var s = data.ops[0].shortUrl;
-            var obj = {"original_url" : l, "short_url" : s}
-            res.send(obj);
-        })
-    });
-
-    //heroku reads this on '/' request, causing console message and unable to read longUrl msg
-    app.get('/:uniqueId', function(req, res) {
-        var uniqueId = req.params.uniqueId;
-        var db = database.db('urlshortdb');
-
-        db.collection('urlshort').find({
-            "uniqueId" : uniqueId
-        }).toArray(function(err, data) {
-            if (err) console.log('Error finding shortened URL');
-            if (data.length == 0) {
-                console.log('No short URL matching this one in the database')
-            }
-            var l = data[0].longUrl;
-            if (l.indexOf("http") == -1) {
-                l = "http://" + l;
-            }
-            res.redirect(l);
+            db.collection('urlshort').insert({
+                "longUrl" : longUrl,
+                "shortUrl" : shortUrl,
+                "uniqueId" : uniqueId
+            }, function(err, data) {
+                if (err) console.log('Error inserting into database');
+                var l = data.ops[0].longUrl;
+                var s = data.ops[0].shortUrl;
+                var obj = {"original_url" : l, "short_url" : s}
+                res.send(obj);
+            })
         });
     });
-}); //end MongoDB connection
+
+    //heroku runs this for '/' and '/new' requests, which causes app to crash.
+    app.get('/:uniqueId', function(req, res) {
+        MongoClient.connect(url, function(err, database) {
+            var uniqueId = req.params.uniqueId;
+            var db = database.db('urlshortdb');
+            
+            db.collection('urlshort').find({
+                "uniqueId" : uniqueId
+            }).toArray(function(err, data) {
+                if (err) console.log('Error finding shortened URL');
+                if (data.length == 0) {
+                    console.log('No short URL matching this one in the database')
+                }
+                var l = data[0].longUrl;
+                if (l.indexOf("http") == -1) {
+                    l = "http://" + l;
+                }
+                res.redirect(l);
+            });
+        });    
+    }); 
 
 var port = process.env.PORT || 3000;
 app.listen(port, () => {
